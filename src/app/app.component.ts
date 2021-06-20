48b1e5ab20d5ed7e915219b060961b2eadef19fd
Rangan BHOWMICK <Rangan.BHOWMICK@amadeus.com>
2021-06-20 19:28:23 +0530

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OAuthService, JwksValidationHandler } from 'angular-oauth2-oidc';
+import { authConfig } from './sso.config';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ui-project';
+
+  constructor(private oauthService:OAuthService){
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn(){
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndLogin();
+  }
+
+
+  get token(){
+    let claims:any = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+import { OAuthModule } from 'angular-oauth2-oidc';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { ProductComponent } from './product/product.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './product.service';
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    DialogBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSortModule,
+    MatPaginatorModule,
+    RouterModule,
+    AppRoutingModule,
+    OAuthModule.forRoot()
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
